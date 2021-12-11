85598a4df97aef9751e01d687ba7f4eea6fd63e6
Ryan Habis <ryan.habiss@gmail.com>
2021-12-11 12:18:59 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        for(int i=2; i<=12; i=i+2)
+        {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        for(int i=3; i<=13; i=i+2)
+        {
+            System.out.println(i);
+        }
+    }
+}
